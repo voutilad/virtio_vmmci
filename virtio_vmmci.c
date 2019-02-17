4b55d012523422a06aeb026c5119160f84b145bf
Dave Voutila <voutilad@gmail.com>
2019-02-17 16:33:38 -0500

    make the module actually have an entry point

--- virtio_vmmci.c
+++ virtio_vmmci.c
@@ -19,10 +19,8 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/virtio.h>
 #include <linux/module.h>
+#include <linux/virtio.h>
 #include "virtio_vmmci.h"
 
 static struct virtio_device_id id_table[] = {
@@ -31,8 +29,14 @@ static struct virtio_device_id id_table[] = {
 };
 
 static unsigned int features[] = {
-	
+
 };
+
+static int __init init(void)
+{
+	printk(KERN_INFO "vmmci init!\n");
+	return 0;
+}
 
 static int vmmci_validate(struct virtio_device *vdev)
 {
@@ -56,23 +60,46 @@ static void vmmci_changed(struct virtio_device *vdev)
 	printk(KERN_INFO "vmmci_change\n");
 }
 
+#ifdef CONFIG_PM_SLEEP
+static int vmmci_freeze(struct virtio_device *vdev)
+{
+	printk(KERN_INFO "vmmci_freeze\n");
+	return 0;
+}
+
+static int vmmci_restore(struct virtio_device *vdev)
+{
+	printk(KERN_INFO "vmmci_restore\n");
+	return 0;
+}
+#endif
+
 static struct virtio_driver virtio_vmmci_driver = {
 	.feature_table = features,
 	.feature_table_size = ARRAY_SIZE(features),
-	.driver.name = KBUILD_MODNAME,
+	.driver.name = 	KBUILD_MODNAME,
 	.driver.owner = THIS_MODULE,
-	.id_table = id_table,
-	.validate = vmmci_validate,
-	.probe = vmmci_probe,
-	.remove = vmmci_remove,
+	.id_table = 	id_table,
+	.validate = 	vmmci_validate,
+	.probe = 	vmmci_probe,
+	.remove = 	vmmci_remove,
 	.config_changed = vmmci_changed,
+#ifdef CONFIG_PM_SLEEP
+	.freeze = 	vmmci_freeze,
+	.restore = 	vmmci_restore,
+#endif
 };
 
-module_virtio_driver(virtio_vmmci_driver);
+static void __exit fini(void)
+{
+	printk(KERN_INFO "vmmci fini!\n");
+}
+
+module_init(init);
+module_exit(fini);
+
 MODULE_DEVICE_TABLE(virtio, id_table);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dave Voutila");
 MODULE_DESCRIPTION("OpenBSD VMM Control Interface");
 MODULE_VERSION("6.4");
-
-
